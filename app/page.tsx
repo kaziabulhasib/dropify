1147c38f8aa246c426d149a7f9b6585c451e3e91
kaziabulhasib <hasibkazi420@gmail.com>
2025-08-20 11:36:17 +0530

    clerk middleware setup

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,9 @@
-import SignUpForm from "@/components/SignUpForm";
 
 export default function Home() {
   return (
     <div className='w-full h-screen flex flex-col items-center justify-center'>
       <h1 className='text-3xl font-bold'>Welcome to dropify</h1>
-      <SignUpForm />
+      
     </div>
   );
 }
